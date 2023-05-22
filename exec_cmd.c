6e6f84a8e3b58ca53251d89844bbcce6fabcfee7
“WhosayOshizzy” <fabulousoshi@gmail.com>
2023-05-22 00:54:49 -0700

    Simple shell

---
+++ exec_cmd.c
@@ -0,0 +1,33 @@
+#include "shell.h"
+/**
+ * exec_cmd - This funct createe a new child process
+ * executes a command, and wait for the child process
+ * to update its status
+ * @c: command
+ * @cmd: vector array of pointers to commands
+ *
+ * Return: void
+ */
+
+void exec_cmd(char *c, char **command)
+{
+	pid_t newProcess;
+	int status;
+	char **envp = environ;
+
+	newProcess = fork();
+	if (newProcess < 0)
+		perror(c);
+	if (newProcess == 0)
+	{
+		execve(c, command, envp);
+		perror(c);
+		free(c);
+		free_cmds(command);
+		exit(98);
+	}
+	else
+	{
+		wait(&status);
+	}
+}
