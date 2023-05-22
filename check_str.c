a46278b8ebe0d1c908986ae6f2da003aa7077507
“WhosayOshizzy” <fabulousoshi@gmail.com>
2023-05-22 00:29:20 -0700

    Simple shell

---
+++ check_str.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+/**
+ * check_str - searches a string for a specific character
+ * @str: string to examine
+ * @chr: character to look for
+ *
+ * Return: pointer to index of first occurence, NULL if not found
+ */
+
+char *check_string(char *str, int chr)
+{
+	while (*str)
+	{
+		if (*str == chr)
+			return (str);
+		str++;
+	}
+	return (NULL);
+}
