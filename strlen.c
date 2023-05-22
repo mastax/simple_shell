8896c725538928eb33c730de2f79ccde5e3cb0e8
“WhosayOshizzy” <fabulousoshi@gmail.com>
2023-05-21 17:25:07 -0700

    Simple shell

---
+++ strlen.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+/**
+ * _strlen - gets the length of a string
+ * @str: string to evaluate
+ *
+ * Return: length of string
+ */
+
+int _strlen(const char *str)
+{
+	size_t i = 0;
+
+	while (str[i])
+		i++;
+	return (i);
+}
